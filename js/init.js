38cad1ad31808be0d2408edf53586f7cb59582ed
Maylo0314 <maylo.elfrink@student.graafschapcollege.nl>
2024-06-05 11:35:53 +0200

    first commit

---
+++ js/init.js
@@ -0,0 +1,32 @@
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+
+const listEl = document.querySelector('.mdc-drawer .mdc-list');
+const mainContentEl = document.querySelector('.main-content');
+
+document.getElementById('navbarButton').addEventListener('click', OpenDrawer);
+
+function OpenDrawer(){
+    drawer.open = true;
+}
+
+function CloseDrawer(){
+    drawer.open = false;
+}
+
+document.getElementById('navbarText').addEventListener('click', removeClass);
+
+function removeClass() {
+    const mainclass = document.querySelectorAll('.mdc-image-list__item');
+    const test = document.querySelectorAll('.mdc-tab, .mdc-tab-indicator');
+    test.forEach(test => {
+        test.classList.remove("mdc-tab--active", "mdc-tab-indicator--active");
+    });
+
+    mainclass.forEach(item => {
+        item.classList.remove("hidden");
+    });
+}
+
